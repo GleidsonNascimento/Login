4462201a5d4188d8d9b036cbf12298782dc2e3fe
GleidsonNascimento <ttt_akashi@hotmail.com>
2023-08-18 20:08:53 -0300

    pequenas autalizações de estilo ainda falta maiores mudanças

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
+import "./login.css";
 
 export default function LoginForm({ togglePage }) {
   const [username, setUsername] = useState("");
@@ -10,25 +11,35 @@ export default function LoginForm({ togglePage }) {
   };
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="nome do usuario"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Senha"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" onClick={HandleLogin}>
-        Login
-      </button>
-      <button>
-        <Link to="/register">registrar</Link>
-      </button>
-    </form>
+    <div className="background">
+      <form className="form-login">
+        <div>
+          <h3>Nome de usuario</h3>
+          <input
+            type="text"
+            placeholder="nome do usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <h3>Senha do usuario</h3>
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="buttons">
+          <button type="submit" onClick={HandleLogin}>
+            Login
+          </button>
+          <button>
+            <Link to="/register">registrar</Link>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
